76af1a33e7e6c67f76108513358771367faaa773
sepiropht <sergembotta@mailoo.org>
2019-06-21 14:41:34 +0200

    project 1: part1

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+
+pub struct KvStore {
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        panic!();
+    }
+    pub fn set(&mut self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
+    }
+
+    pub fn get(&self, query: String) -> Option<String> {
+        panic!();
+    }
+}
