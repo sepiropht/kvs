c14d7e59e3b0324858ca1016d58b0c89fa6b6b3b
sepiropht <sergembotta@mailoo.org>
2019-07-31 15:51:36 +0200

    implementing set

--- project-2/src/kvs.rs
+++ project-2/src/kvs.rs
@@ -2,10 +2,17 @@ use std::collections::HashMap;
 use std::process;
 pub use crate::{KvsError, Result};
 use std::path::Path;
+use std::fs;
+use std::io::Write;
+use serde::{Serialize, Deserialize};
+
 
 pub struct KvStore {
     store: HashMap<String, String>,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Log(String, String);
 
 impl KvStore {
     pub fn new() -> Self {
@@ -14,7 +21,29 @@ impl KvStore {
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        unimplemented!();
+        let point = Log(key, value);
+
+            // Convert the Point to a JSON string.
+        let serialized = serde_json::to_string(&point).unwrap();
+           
+                    // Prints serialized = {"x":1,"y":2}
+        println!("serialized = {}", serialized);
+            
+                             // Convert the JSON string back to a Point.
+            //                     let deserialized: Point =
+        //serde_json::from_str(&serialized).unwrap();
+            
+
+        let mut log = fs::File::create("log").unwrap();
+
+        match log.write(serialized.as_bytes()) {
+            Ok(_) => process::exit(0),
+            Err(err) => {
+                eprintln!("{}", err);
+                process::exit(1);
+            },
+        };
+                                         
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
